75fe1edbda551a73253f44af414e5776188a8cd3
avavion <avavionmvm@gmail.com>
2020-09-10 14:14:53 +0300

    ReactJS init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const comment = {
+  date: new Date(),
+  text: "Пробую себя в роли React разработчика",
+  author: {
+    name: "Алан Богов",
+    avatarUrl:
+      "https://sun7-8.userapi.com/c637230/v637230312/d112/tazrMS_j_eM.jpg",
+  },
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div>{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <h1 className="UserInfo-name">{props.user.name}</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
+      <Clock></Clock>
+    </div>
+  );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Привет, мир!</h1>
+        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 serviceWorker.unregister();
